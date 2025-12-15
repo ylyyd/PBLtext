f5e073fa70130af73abfcf4114a973e5b42ac3fe
y2003 <3057776373@qq.com>
2025-12-15 10:54:38 +0800

    提交文件1

---
+++ src/main/java/entity/Administrator.java
@@ -0,0 +1,57 @@
+package entity;
+
+/**
+ * 表示数据库中的表librarian各个属性
+ */
+public class Administrator {
+	private int id;
+	private String name;
+	private String password;
+
+	public Administrator() {
+	}
+
+	/**
+	 * 初始化administrator的属性
+	 * 
+	 * @param id
+	 * @param name
+	 * @param password
+	 */
+	public Administrator(int id, String name, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", password=" + password + "]";
+	}
+
+}
---
+++ src/main/java/entity/Cart.java
@@ -0,0 +1,66 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * 显示读者的借阅车的消息 增加了bookName、readerId、readerName;
+ */
+public class Cart {
+
+	private int bookId;
+	private String bookName;
+	private int readerId;
+	private String readerName;
+	private Date submitTime; // 当reader把借阅车中的的书提交借阅是，添加时间
+
+	public Cart() {
+
+	}
+
+	public int getReaderId() {
+		return readerId;
+	}
+
+	public void setReaderId(int readerId) {
+		this.readerId = readerId;
+	}
+
+	public String getReaderName() {
+		return readerName;
+	}
+
+	public void setReaderName(String readerName) {
+		this.readerName = readerName;
+	}
+
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Date getSubmitTime() {
+		return submitTime;
+	}
+
+	public void setSubmitTime(Date submitTime) {
+		this.submitTime = submitTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [bookId=" + bookId + ", bookName=" + bookName + ", readerId=" + readerId + ", readerName="
+				+ readerName + ", submitTime=" + submitTime + "]";
+	}
+
+}
