8e2c94050077bce0c4026d648d204d9c77fc8030
y2003 <3057776373@qq.com>
2025-12-21 21:40:51 +0800

    更新

--- src/main/java/action/DestorySession.java
+++ src/main/java/action/DestorySession.java
@@ -1,5 +1,7 @@
+// 定义包路径
 package action;
 
+// 导入所需的Java类库
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,12 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class DestorySession
+ * Servlet实现类 DestorySession
+ * 用于销毁用户会话(session)，实现用户登出功能
  */
 public class DestorySession extends HttpServlet {
+	// 序列化版本号，用于序列化兼容性检查
 	private static final long serialVersionUID = 1L;
 
 	/**
+	 * 构造函数
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public DestorySession() {
@@ -21,14 +26,17 @@ public class DestorySession extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * 处理所有HTTP请求的方法(service)
+	 * 主要功能是销毁当前用户的会话并重定向到主页
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		// 获取当前用户的会话对象
 		HttpSession session = request.getSession();
+		// 销毁会话，清除所有存储在会话中的数据
 		session.invalidate();
+		// 重定向用户到主页(homepage.jsp)
 		response.sendRedirect("homepage.jsp");
 	}
-
 }
